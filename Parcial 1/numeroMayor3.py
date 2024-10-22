aa78463ac6016c9f825f00bc643eaca07d50004a
EEllisonJMLTP11 <LTPw11>
2024-10-22 16:49:03 -0600

    Solucion codigo examen

---
+++ Parcial 1/numeroMayor3.py
@@ -0,0 +1,13 @@
+# Entradas
+A = input("A : ")
+B = input("B : ")
+C = input("C : ")
+
+# Procesos
+if A > B and A > C:
+    print(A," ES EL MAYOR") # Salida
+else:
+    if B > A and B > C:
+        print(B," ES EL MAYOR") # Salida
+    else:
+        print(C," ES EL MAYOR") # Salida
